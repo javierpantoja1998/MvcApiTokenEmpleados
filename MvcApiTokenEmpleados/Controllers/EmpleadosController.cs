da7ee127e02831942b49bd6521b2312e6e152870
javierpantoja1998 <cientificodebanco@gmail.com>
2023-04-28 10:53:23 +0200

    Add project files.

---
+++ MvcApiTokenEmpleados/Controllers/EmpleadosController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApiTokenEmpleados.Models;
+using MvcApiTokenEmpleados.Services;
+
+namespace MvcApiTokenEmpleados.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private ServiceApiEmpleados service;
+
+        public EmpleadosController(ServiceApiEmpleados service)
+        {
+            this.service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            string token =
+                HttpContext.Session.GetString("TOKEN");
+            if(token == null)
+            {
+                ViewData["MENSAJE"] = "DEBE REALIZAR LOGIN PARA VISUALIZAR DATOS";
+                return View();
+            }
+            else
+            {
+                List<Empleado> empleados =
+                    await this.service.GetEmpleadosAsync(token);
+                return View(empleados);
+            }
+            
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            Empleado emp = await this.service.FindEmpleadoAsync(id);
+            return View(emp);
+        }
+    }
+}
